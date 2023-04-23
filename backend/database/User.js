78275d2a4349b79b5cf9e9e94770d70c1a9cfa85
Meghna-J <96060532+Meghna-J@users.noreply.github.com>
2023-04-23 15:45:36 +0530

    Add files via upload

---
+++ backend/database/User.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+// mongoose.connect('mongodb://127.0.0.1:27017/usersdb');
+const UserSchema = new mongoose.Schema({
+    email:String,
+    password:String
+})
+
+module.exports = mongoose.model('user',UserSchema);
